fd298dee1f11598e0d49aba2f39e592950168fa2
Francis Martinez <f.martinez1993@icloud.com>
2025-08-30 17:20:38 -0400

    New styles to tailwindStyles.js

--- src/importFiles/tailwindStyles.js
+++ src/importFiles/tailwindStyles.js
@@ -1,9 +1,17 @@
 const inputs = "h-16px";
 const icons = "mr-2 inline-block h-5 w-5 align-[-2px] text-[var(--textColor)]";
+const badgeGreen = "px-1 rounded-3xl whitespace-nowrap text-[var(--greenMain)] bg-[var(--green-trasparent)] border border-[var(--greenMain) font-bold text-[12px]";
+const badgeYellow = "px-1 rounded-3xl whitespace-nowrap text-[var(--yellow)] bg-[var(--yellow-trasparent)] border border-[var(--yellow) font-bold text-[12px]";
+const baggeOrange = "px-1 rounded-3xl whitespace-nowrap text-[var(--orange)] bg-[var(--orange-trasparent)] border border-[var(--orange) font-bold text-[12px]";
 
 const tailwindClass = {
     inputs: inputs,
     icons: icons,
+    badge: {
+        green: badgeGreen,
+        yellow: badgeYellow,
+        orange: baggeOrange
+    },
 }
 
 export {inputs, tailwindClass};
