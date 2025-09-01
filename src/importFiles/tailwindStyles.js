f9c509cc195cef4d462d8d5af860426594f0b94d
Francis Martinez <f.martinez1993@icloud.com>
2025-09-01 14:54:26 -0400

    Fix: heigh to the StatutsBadge.jxs on tailwindStyles.js adding sefl-stre-stretch resolve the issue

--- src/importFiles/tailwindStyles.js
+++ src/importFiles/tailwindStyles.js
@@ -7,8 +7,8 @@ const badgeGreen = " px-2 rounded-3xl whitespace-nowrap text-[var(--greenMain)] 
 const badgeYellow = " rounded-3xl whitespace-nowrap text-[var(--yellow)] bg-[var(--yellow-trasparent)] border border-[var(--yellow) font-bold text-[12px]";
 const badgeOrange = " rounded-3xl whitespace-nowrap text-[var(--orange)] bg-[var(--orange-trasparent)] border border-[var(--orange) font-bold text-[12px]";
 
-const stsBadge = "rounded-t-lg border border-slate-700/50 flex items-center overflow-hidden pr-1 min-w-25  min-h-6 gap-1 h-6"
-const stsBadgeChildren_1 = "bg-black text-white h-full flex items-center justify-center w-7"
+const stsBadge = "rounded-t-lg border border-slate-700/50 flex items-center overflow-hidden pr-1 min-w-25  min-h-6 gap-1 "
+const stsBadgeChildren_1 = "bg-black text-white self-stretch flex items-center justify-center w-7"
 const stsBadgeChildren_2 = " text-[var(--textColor)] capitalize font-semibold text-xs"
 
 const priorBadge = "rounded-b-lg border border-slate-700/50 flex items-center overflow-hidden pr-1 min-w-25  min-h-6 gap-1 h-6"
