fd1d98ad38ef89b14b8b3d85a177d923be5d02a8
Francis Martinez <f.martinez1993@icloud.com>
2025-09-01 13:55:38 -0400

    Adding new Style to priority badge

--- src/Utils/PriorityBadge.jsx
+++ src/Utils/PriorityBadge.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import { SVGIcons } from "../importFiles/imports";
+import { tailwindClass } from "../importFiles/tailwindStyles";
 
 function PriorityBadge({ task }) {
   return (
     <div
-      className={`col-span-1  w-10 h- flex items-center justify-center rounded-3xl `}
+      className={
+        tailwindClass.priority.parent +
+        (task.priority === "low"
+          ? " bg-[var(--green-trasparent)]"
+          : task.priority === "medium"
+          ? " bg-[var(--yellow-trasparent)]"
+          : task.priority === "high"
+          ? " bg-[var(--orange-trasparent)]"
+          : "")
+      }
     >
-      {task.priority === "high" ? (
-        <SVGIcons.priority.high className="text-[var(--orange)] text-lg" />
-      ) : task.priority === "medium" ? (
-        <SVGIcons.priority.med className="text-[var(--yellow)]" />
-      ) : task.priority === "low" ? (
-        <SVGIcons.priority.low className="text-[var(--greenMain)]" />
-      ) : (
-        <SVGIcons.question />
-      )}
+      <div className={tailwindClass.priority.children1}>
+        {task.priority === "high" ? (
+          <SVGIcons.priority.high className="text-[var(--orange)] text-lg" />
+        ) : task.priority === "medium" ? (
+          <SVGIcons.priority.med className="text-[var(--yellow)]" />
+        ) : task.priority === "low" ? (
+          <SVGIcons.priority.low className="text-[var(--greenMain)]" />
+        ) : (
+          <SVGIcons.question />
+        )}
+      </div>
+      <spa className={tailwindClass.priority.children2}>{task.priority}</spa>
     </div>
   );
 }
--- src/importFiles/tailwindStyles.js
+++ src/importFiles/tailwindStyles.js
@@ -6,9 +6,14 @@ const iconsBlack = "inline-block h-5 w-5 align-[-2px] text-black w-10 h-7";
 const badgeGreen = " px-2 rounded-3xl whitespace-nowrap text-[var(--greenMain)] bg-[var(--green-trasparent)] border border-[var(--greenMain) font-bold text-[12px]";
 const badgeYellow = " rounded-3xl whitespace-nowrap text-[var(--yellow)] bg-[var(--yellow-trasparent)] border border-[var(--yellow) font-bold text-[12px]";
 const badgeOrange = " rounded-3xl whitespace-nowrap text-[var(--orange)] bg-[var(--orange-trasparent)] border border-[var(--orange) font-bold text-[12px]";
+
 const stsBadge = "rounded-lg border border-slate-700/50 flex items-center overflow-hidden pr-1 min-w-25  min-h-6 gap-1 h-6"
 const stsBadgeChildren_1 = "bg-black text-white h-full flex items-center justify-center w-7"
 const stsBadgeChildren_2 = " text-[var(--textColor)] capitalize font-semibold text-xs"
+
+const priorBadge = "rounded-lg border border-slate-700/50 flex items-center overflow-hidden pr-1 min-w-25  min-h-6 gap-1 h-6"
+const priorBadgeChildren_1 = "bg-black text-white h-full flex items-center justify-center w-7"
+const priorBadgeChildren_2 = " text-[var(--textColor)] capitalize font-semibold text-xs"
 
 const tailwindClass = {
     inputs: inputs,
@@ -27,6 +32,11 @@ const tailwindClass = {
         children1: stsBadgeChildren_1,
         children2: stsBadgeChildren_2,
     },
+    priority:{
+        parent: priorBadge,
+        children1: priorBadgeChildren_1,
+        children2: priorBadgeChildren_2
+    }
 }
 
 export {tailwindClass};
